14c1f98caf1fc97d2179decb936950d3c895e5c6
Kelven Souza <kelven.souza00@gmail.com>
2022-05-15 22:10:05 -0300

    Correção itens posição

---
+++ index.js
@@ -0,0 +1,8 @@
+import { data } from "./database.js";
+headerCreate()
+function headerCreate(){
+    let header = document.createElement('header')
+    document.querySelector('body').appendChild(header)
+    let nav = document.createElement('nav')
+    
+}
